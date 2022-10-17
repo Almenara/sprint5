901aa878c439ea1cc012500964f88d6181b3fb3d
albertoalmenara <PeopleRK>
2022-10-17 17:38:20 +0200

    Sprint 5 / Ejercicio 3

--- index.ts
+++ index.ts
@@ -6,21 +6,48 @@ const header:object ={
   },
 }
 
+let scoringButtons: HTMLElement|null = document.getElementById('scoring-buttons');
+let nextJokeButton: HTMLElement|null = document.getElementById('next-joke-button');
+
+let currentJoke:string;
+let reportJokes:object[] = [];
+
 const getJoke = () => { 
+
+  nextJokeButton?.setAttribute('disabled', '');
+
   fetch(urlJoke,header)
   .then((resolve) => {
-    if (resolve.ok) {
-      
-      let joke:string; 
+    if (resolve.ok) {    
       return resolve.json();
     }
     throw {
       statusText: resolve.statusText,
       status: resolve.status
     };
+
   })
   .then((request) => {
-    document.getElementById('joke-text')!.innerHTML = request.joke;
+
+    currentJoke = request.joke;
+    document.getElementById('joke-text')!.innerHTML = currentJoke;
+    nextJokeButton?.classList.add('d-none');
+    scoringButtons?.classList.remove('d-none');
+    nextJokeButton?.removeAttribute('disabled');
+
   })
-  .catch((reject) => console.log(reject));
+  .catch((reject) => {
+
+    console.log(reject)
+    nextJokeButton?.removeAttribute('disabled');
+
+  });
+}
+const jokeScore = (score:number) => {
+  
+  reportJokes.push({joke: currentJoke, score: score, date: new Date().toISOString()})
+  scoringButtons?.classList.add('d-none');
+  nextJokeButton?.classList.remove('d-none');
+  console.log(reportJokes);
+
 }
