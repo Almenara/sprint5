21cf2a83f2adc1d5d3a159d2d073f97a28aaaab2
albertoalmenara <PeopleRK>
2022-10-17 21:07:32 +0200

    Sprint 5 / Ejercicio 4

--- index.ts
+++ index.ts
@@ -8,6 +8,8 @@ const header:object ={
 
 let scoringButtons: HTMLElement|null = document.getElementById('scoring-buttons');
 let nextJokeButton: HTMLElement|null = document.getElementById('next-joke-button');
+let weatherIcon: HTMLElement|null = document.getElementById('weather-icon');
+let temperature: HTMLElement|null = document.getElementById('temperature');
 
 let currentJoke:string;
 let reportJokes:object[] = [];
@@ -38,7 +40,7 @@ const getJoke = () => {
   })
   .catch((reject) => {
 
-    console.log(reject)
+    console.error(reject)
     nextJokeButton?.removeAttribute('disabled');
 
   });
@@ -51,3 +53,47 @@ const jokeScore = (score:number) => {
   console.log(reportJokes);
 
 }
+
+let urlWeater:string = "https://icanhazdadjoke.com/";
+
+let getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
+
+let getWeather = () =>{
+  fetch(urlWeater,header)
+  .then((resolve) => {
+    if (resolve.ok) {    
+      return resolve.json();
+    }
+    throw {
+      statusText: resolve.statusText,
+      status: resolve.status
+    };
+
+  })
+  .then((request) => {
+    
+    console.log(request.current_weather);
+    weatherIcon!.setAttribute('src', `/src/img/${request.current_weather.weathercode}.svg`);
+    temperature!.innerHTML = request.current_weather.temperature;
+    
+  })
+  .catch((reject) => console.error(reject));
+}
+
+let weather = () =>{
+  getPosition()
+  .then((position:any) => {
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+    urlWeater = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&daily=weathercode&current_weather=true&timezone=Europe%2FBerlin`;
+    getWeather();
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
+}
+weather()
--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,8 @@ const header = {
 };
 let scoringButtons = document.getElementById('scoring-buttons');
 let nextJokeButton = document.getElementById('next-joke-button');
+let weatherIcon = document.getElementById('weather-icon');
+let temperature = document.getElementById('temperature');
 let currentJoke;
 let reportJokes = [];
 const getJoke = () => {
@@ -30,7 +32,7 @@ const getJoke = () => {
         nextJokeButton === null || nextJokeButton === void 0 ? void 0 : nextJokeButton.removeAttribute('disabled');
     })
         .catch((reject) => {
-        console.log(reject);
+        console.error(reject);
         nextJokeButton === null || nextJokeButton === void 0 ? void 0 : nextJokeButton.removeAttribute('disabled');
     });
 };
@@ -40,3 +42,40 @@ const jokeScore = (score) => {
     nextJokeButton === null || nextJokeButton === void 0 ? void 0 : nextJokeButton.classList.remove('d-none');
     console.log(reportJokes);
 };
+let urlWeater = "https://icanhazdadjoke.com/";
+let getPosition = function () {
+    return new Promise(function (resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+};
+let getWeather = () => {
+    fetch(urlWeater, header)
+        .then((resolve) => {
+        if (resolve.ok) {
+            return resolve.json();
+        }
+        throw {
+            statusText: resolve.statusText,
+            status: resolve.status
+        };
+    })
+        .then((request) => {
+        console.log(request.current_weather);
+        weatherIcon.setAttribute('src', `/src/img/${request.current_weather.weathercode}.svg`);
+        temperature.innerHTML = request.current_weather.temperature;
+    })
+        .catch((reject) => console.error(reject));
+};
+let weather = () => {
+    getPosition()
+        .then((position) => {
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
+        urlWeater = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&daily=weathercode&current_weather=true&timezone=Europe%2FBerlin`;
+        getWeather();
+    })
+        .catch((err) => {
+        console.error(err.message);
+    });
+};
+weather();
